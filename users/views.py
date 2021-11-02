fbecf883330ad99160a7f380cc53e20a4b9fc9ab
blingblin-g <luvbliny@gmail.com>
2021-11-03 03:14:57 +0900

    FIX : django orm to pymongo

--- users/views.py
+++ users/views.py
@@ -5,8 +5,11 @@ from django.http import JsonResponse
 from datetime import datetime, timedelta
 
 from my_settings import SECRET_KEY, ALGORITHM
-from users.models import User
+from utils import get_db_handle
 
+db_handle, client = get_db_handle()
+
+user_collection = db_handle['user']
 
 class SignUpView(View):
     def post(self, request):
@@ -16,7 +19,7 @@ class SignUpView(View):
 
         password = data["password"]
 
-        if User.objects.filter(email = data["email"]).exists():
+        if user_collection.find_one({"email": data["email"]}):
             return JsonResponse({"MESSAGE" : "DUPLICATED EMAIL"}, status = 400)
 
         if not REGEX_EMAIL.match(data["email"]):
@@ -28,12 +31,12 @@ class SignUpView(View):
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
         decoded_password = hashed_password.decode("utf-8")
 
-        User.objects.create(
-            name = data["name"], 
-            email = data["email"],
-            password = decoded_password
-        )
-
+        new_user = {
+            "name": data["name"],
+            "email": data["email"],
+            "password": decoded_password,
+        }
+        user_collection.insert_one(new_user)
         return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
 
 
@@ -42,16 +45,15 @@ class SignInView(View):
         data = json.loads(request.body)
 
         try:
-            if not User.objects.filter(email = data["email"]).exists():
+            user = user_collection.find_one({"email": data["email"]})
+            if not user:
                 return JsonResponse({"MESSAGE": "INVALID_USER"}, status = 401)
 
-            user = User.objects.get(email = data["email"])
-
-            if not bcrypt.checkpw(data["password"].encode("utf-8"), user.password.encode("utf-8")):
+            if not bcrypt.checkpw(data["password"].encode("utf-8"), user["password"].encode("utf-8")):
                 return JsonResponse({"MESSAGE": "INVALID_PASSWORD"}, status = 401)
-            
-            access_token = jwt.encode({"id": user.id , 'exp':datetime.utcnow() + timedelta(days=3)}, SECRET_KEY , algorithm=ALGORITHM)
-            return JsonResponse({"MESSAGE": "SUCCESS", 'token' : access_token, "user_name" : user.name}, status = 200)
+
+            access_token = jwt.encode({"id": str(user["_id"]) , 'exp':datetime.utcnow() + timedelta(days=3)}, SECRET_KEY , algorithm=ALGORITHM)
+            return JsonResponse({"MESSAGE": "SUCCESS", 'token' : access_token, "user_name" : user["name"]}, status = 200)
 
         except KeyError:
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
